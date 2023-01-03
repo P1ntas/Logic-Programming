e489c7764ea83515da091383140e9a38cb27893b
AfonsoAbreu1922 <up202008552@edu.fe.up.pt>
2023-01-03 02:33:02 +0000

    changes on list of directions and bot.pl

---
+++ src/Taacoca.pl
@@ -0,0 +1,27 @@
+:- consult('display.pl').
+:- consult('play.pl').
+:- consult('input.pl').
+:- consult('logic.pl').
+:- consult('bot.pl').
+:- consult('utils.pl').
+
+:-[input], [play], [display], [logic], [bot].
+:-use_module(library(random)).
+:-use_module(library(clpfd)).
+:-use_module(library(lists)).
+:-use_module(library(system)).
+:- use_module(library(random)).
+
+
+% Main function
+% Calls the initial functions
+
+% start the game
+play :-
+    repeat,
+    display_menu,
+    read_number(0, 3, Input),
+    handle_input(Input).
+
+=======
+% To play the game, you can simply run the play predicate in the Prolog interpreter. The game will then prompt you to select which stones to move, and the computer will make its moves automatically. The game will continue until one of the players wins or there are no more moves left.
